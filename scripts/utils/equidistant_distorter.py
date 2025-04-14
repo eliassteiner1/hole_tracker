3fc6c0352b191c9a69d2006022edf26b7aa5207e
Elias Steiner <elias.steiner@gmx.ch>
2025-04-14 12:58:09 +0200

    docstring cleanup

--- scripts/utils/equidistant_distorter.py
+++ scripts/utils/equidistant_distorter.py
@@ -5,8 +5,14 @@ import cv2
 
 class EquidistantDistorter:
     def __init__(self, k1: float, k2: float, k3: float, k4: float):
-        """
-        set the camera calibration parameters k1-4 from the equidistant distortion model for wide-angle lenses
+        """ set the camera calibration parameters k1-4 from the equidistant distortion model for wide-angle lenses. Seems to be the Kannala-Brandt distortion model implemented in Kalibr.
+        
+        args
+        ----
+        - `k1`: distortion model parameter
+        - `k2`: distortion model parameter
+        - `k3`: distortion model parameter
+        - `k4`: distortion model parameter
         """
         
         self.k1 = k1
@@ -16,23 +22,24 @@ class EquidistantDistorter:
         return
 
     def distort(self, point: tuple):
-        """
-        [P normalized image plane] ==> [P distorted image plane]
-        Apply equidistant distortion to a 2D point in the normalized image plane.
+        """ [P normalized image plane] ==> [P distorted image plane]. Apply equidistant distortion to a 2D point in the normalized image plane.
         
-        Args:
-            point: (X/Z, Y/Z) from [X, Y, Z]/Z (the 1 is left out, but only input normalized coords!)
-        Returns:
-            point: (X/Z', Y/Z') distorted point in normalized image plane
+        args
+        ----
+        - `point`: (X/Z, Y/Z) from [X, Y, Z]/Z (the 1 is left out, but only input normalized coords!)
+        
+        returns
+        -------
+        - `point`: (X/Z', Y/Z') distorted point in normalized image plane
         """
         
         x, y = point
-        r = np.sqrt(x**2 + y**2)
+        r    = np.sqrt(x**2 + y**2)
         
         if r < 1e-8:
             return np.array([x, y])  # No distortion for small radii
 
-        theta = np.arctan(r)
+        theta  = np.arctan(r)
         theta2 = theta * theta
         theta4 = theta2 * theta2
         theta6 = theta4 * theta2
@@ -45,25 +52,26 @@ class EquidistantDistorter:
         return np.array([x * scaling, y * scaling])
 
     def undistort(self, point: tuple, iterations: int=20):
-        """
-        [P distorted image plane] ==> [P normalized image plane]
-        Remove equidistant distortion from a 2D point in the distorted image plane.
+        """ [P distorted image plane] ==> [P normalized image plane]. Remove equidistant distortion from a 2D point in the distorted image plane.
 
-        Args:
-            point: (X/Z', Y/Z') in distorted normalized image plane coords 
-        Returns:
-            point (X/Z, Y/Z) from [X, Y, Z]/Z (the 1 is left out, but only input normalized coords!)
+        args
+        ----
+        - `point`: (X/Z', Y/Z') in distorted normalized image plane coords 
+        
+        returns
+        -------
+        - `point`: (X/Z, Y/Z) from [X, Y, Z]/Z (the 1 is left out, but only input normalized coords!)
         """
         
-        x, y = point
+        x, y   = point
         thetad = np.sqrt(x**2 + y**2)
 
         if thetad < 1e-8:
-            return np.array([x, y])  # No need to undistort near the center
+            return np.array([x, y])  # no need to undistort near the center
 
-        theta = thetad  # Initial guess
+        theta = thetad  # initial guess
 
-        # Iterative undistortion
+        # iterative undistortion
         for _ in range(iterations):
             theta2 = theta  * theta
             theta4 = theta2 * theta2
